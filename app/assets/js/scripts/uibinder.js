08fffedd57862a7ca1f887adee3af133242e3b00
Manon B. <72684309+Plhume@users.noreply.github.com>
2024-04-14 11:08:40 +0200

    Update uibinder.js

--- app/assets/js/scripts/uibinder.js
+++ app/assets/js/scripts/uibinder.js
@@ -94,8 +94,8 @@ async function showMainUI(data) {
     refreshServerStatus()
     setTimeout(() => {
         document.getElementById('frameBar').style.backgroundColor = 'rgba(0, 0, 0, 0) 100%'
-        document.body.style.backgroundColor = `rgba(0, 0, 0, 0) 100%`
-        //document.body.style.backgroundImage = `url('assets/images/backgrounds/${document.body.getAttribute('bkid')}.jpg')`
+        //document.body.style.backgroundColor = `rgba(0, 0, 0, 0) 100%`
+        document.body.style.backgroundImage = `url('assets/images/backgrounds/${document.body.getAttribute('bkid')}.jpg')`
         $('#main').show()
 
         const isLoggedIn = Object.keys(ConfigManager.getAuthAccounts()).length > 0
