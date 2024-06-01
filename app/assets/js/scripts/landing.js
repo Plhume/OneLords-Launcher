dded5fabc3b150c386bdc9894fe43d7529b5a912
Manon B. <72684309+Plhume@users.noreply.github.com>
2024-06-01 12:16:04 +0200

    Resolve error
    Resolving the error who makes no launching game

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -57,7 +57,7 @@ function setLaunchDetails(details) {
 function setLaunchPercentage(percent) {
     //launch_progress.setAttribute('max', 100)
     //launch_progress.setAttribute('value', percent)
-    launch_progress_label.innerHTML = percent + '%'
+    //launch_progress_label.innerHTML = percent + '%'
 }
 
 function setDownloadPercentage(percent) {
